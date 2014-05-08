76e050e1a0d379bc31509f1f4af0162d2c1d6ed3
Mark Gilbert <mgilbert@dropbox.com>
2014-05-08 13:28:59 -0700

    setup default database

--- project/config.py
+++ project/config.py
@@ -7,7 +7,7 @@ SECRET_KEY = "awef98awefinq23r9wkladsf98q2jkl3rq2"
 # Database Info
 SQLALCHEMY_ECHO = False
 SQL_HOSTNAME = 'localhost'
-SQL_DB_NAME = 'project'
+SQL_DB_NAME = 'development'
 SQL_USERNAME = 'dropbox'
 SQL_PASSWORD = 'dropbox'
 SQLALCHEMY_DATABASE_URI = 'mysql://%s:%s@%s/%s?charset=utf8&use_unicode=1' % (SQL_USERNAME, SQL_PASSWORD, SQL_HOSTNAME, SQL_DB_NAME)
--- vm_setup.sh
+++ vm_setup.sh
@@ -4,3 +4,7 @@ ln -fs /vagrant /srv/project
 
 cd  /srv/project
 sudo pip install -r requirements.txt
+
+mysql -u root -e "create database development;"
+mysql -u root -e "create user 'dropbox'@'localhost' identified by 'dropbox';"
+mysql -u root -e "grant all on development.* to 'dropbox'@'localhost';"
